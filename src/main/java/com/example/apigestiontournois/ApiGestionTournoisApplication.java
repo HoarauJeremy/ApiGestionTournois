d82a6a6417e0a6c342ba728e79f639875431b0e6
jeremy <jeremyhoarau44@gmail.com>
2025-05-14 13:55:24 +0400

    Premier Commit

---
+++ src/main/java/com/example/apigestiontournois/ApiGestionTournoisApplication.java
@@ -0,0 +1,13 @@
+package com.example.apigestiontournois;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGestionTournoisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGestionTournoisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/apigestiontournois/ApiGestionTournoisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apigestiontournois;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGestionTournoisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
