46e96bb3f534dfbd480f2a2141f51794a015e134
jeremy <jeremyhoarau44@gmail.com>
2025-05-14 15:27:12 +0400

    Modification de TournoisController et ajout de TournoisRepository

--- src/main/java/com/example/apigestiontournois/controller/TournoisController.java
+++ src/main/java/com/example/apigestiontournois/controller/TournoisController.java
@@ -1,4 +1,77 @@
 package com.example.apigestiontournois.controller;
 
+import com.example.apigestiontournois.model.Tournois;
+import com.example.apigestiontournois.repository.TournoisRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("tournois")
 public class TournoisController {
+
+    private TournoisRepository tournoisRepository;
+
+    @GetMapping
+    public ResponseEntity<List<Tournois>> getAllTournois(){
+        return new ResponseEntity<>((List<Tournois>) tournoisRepository.findAll(), HttpStatus.OK);
+    }
+
+    /**
+     * @param id Identifiant du tournois
+     * @return Les informations sur un tournois
+     */
+    @GetMapping("/{id}")
+    public ResponseEntity<Tournois> getTournoisById(@PathVariable Long id){
+        if(tournoisRepository.findById(id).isPresent()){
+            return new ResponseEntity<>(tournoisRepository.findById(id).get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping
+    @Transactional
+    public ResponseEntity<Tournois> createTournois(@RequestBody Tournois tournois){
+        if (tournoisRepository.findById(tournois.getId()).isPresent()){
+            tournoisRepository.save(tournois);
+            return new ResponseEntity<>(tournois, HttpStatus.CREATED);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PutMapping("/{id}")
+    @Transactional
+    public ResponseEntity<Tournois> updateTournois(@PathVariable Long id, @RequestBody Tournois tournois) {
+        if(tournoisRepository.findById(id).isPresent()){
+
+            Tournois tournoisUpdate = tournoisRepository.findById(id).get();
+
+            tournoisUpdate.setNom(tournois.getNom());
+            tournoisUpdate.setDate_debut(tournois.getDate_debut());
+            tournoisUpdate.setDate_fin(tournois.getDate_fin());
+
+            tournoisRepository.save(tournoisUpdate);
+
+            return new ResponseEntity<>(tournois, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    public ResponseEntity<Tournois> deleteTournois(@PathVariable Long id){
+        if(tournoisRepository.findById(id).isPresent()){
+            tournoisRepository.deleteById(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
 }
--- src/main/java/com/example/apigestiontournois/repository/TournoisRepository.java
+++ src/main/java/com/example/apigestiontournois/repository/TournoisRepository.java
@@ -2,6 +2,8 @@ package com.example.apigestiontournois.repository;
 
 import com.example.apigestiontournois.model.Tournois;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
-interface TournoisRepository extends CrudRepository<Tournois, Long> {
+@Repository
+public interface TournoisRepository extends CrudRepository<Tournois, Long> {
 }
