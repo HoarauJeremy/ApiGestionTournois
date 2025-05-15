f0fb5647267d94fa44d9508ed406320db4cbaa0d
jeremy <jeremyhoarau44@gmail.com>
2025-05-15 09:21:26 +0400

    Suppression des fichier DoubleRepository, SimpleRepository et TournoisController

--- src/main/java/com/example/apigestiontournois/repository/DoubleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.apigestiontournois.repository;
-
-import com.example.apigestiontournois.model.DoubleTournois;
-import org.springframework.data.repository.CrudRepository;
-
-interface DoubleRepository extends CrudRepository<DoubleTournois, Long> {
-}
--- src/main/java/com/example/apigestiontournois/repository/SimpleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.apigestiontournois.repository;
-
-import com.example.apigestiontournois.model.SimpleTournois;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SimpleRepository extends CrudRepository<SimpleTournois, Long> {
-}
