7efd9b16d10cd47a0f5ac5f2433c30e741dbb6d9
jeremy <jeremyhoarau44@gmail.com>
2025-05-20 08:37:35 +0400

    Modification de .gitignore, de ChampionnatRepository et DoubleRepository

--- src/main/java/com/example/apigestiontournois/repository/ChampionnatRepository.java
+++ src/main/java/com/example/apigestiontournois/repository/ChampionnatRepository.java
@@ -2,6 +2,8 @@ package com.example.apigestiontournois.repository;
 
 import com.example.apigestiontournois.model.ChampionnatTournois;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 interface ChampionnatRepository extends CrudRepository<ChampionnatTournois, Long> {
 }
--- src/main/java/com/example/apigestiontournois/repository/DoubleRepository.java
+++ src/main/java/com/example/apigestiontournois/repository/DoubleRepository.java
@@ -2,6 +2,8 @@ package com.example.apigestiontournois.repository;
 
 import com.example.apigestiontournois.model.DoubleTournois;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 interface DoubleRepository extends CrudRepository<DoubleTournois, Long> {
 }
